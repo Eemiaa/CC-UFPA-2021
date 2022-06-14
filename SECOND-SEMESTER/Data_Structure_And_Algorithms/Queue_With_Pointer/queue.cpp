cdb371267b1884759779f3db3d56474b0bd85062
Aimeê Miranda <aimeemiranribe@gmail.com>
2022-06-13 23:14:25 -0300

    new features

--- SECOND-SEMESTER/Data_Structure_And_Algorithms/Queue_With_Pointer/queue.cpp
+++ SECOND-SEMESTER/Data_Structure_And_Algorithms/Queue_With_Pointer/queue.cpp
@@ -27,13 +27,57 @@ void enfileirar(TPnodo x, TPfila* fila){
     fila->tras->proximo = NULL;
 }
 
+void fura_fila_enfileira(TPnodo x, TPfila* fila, int posicao){
+    apontador p = (apontador) malloc(sizeof(Celula));
+    apontador q = (apontador) malloc(sizeof(Celula)); 
+    p=fila->frente;
+    for(int i=1; i<posicao; i++){
+        p = p->proximo;
+    }  
+    q = p->proximo;
+    p->proximo= (apontador) malloc(sizeof(Celula));
+    p->proximo->nodo=x;
+    p->proximo->proximo = q;  
+}
+
+void fura_fila_desenfileira(TPfila* fila, int posicao){
+    apontador p = (apontador) malloc(sizeof(Celula));
+    apontador q; 
+    p=fila->frente;
+    for(int i=1; i<posicao; i++){
+        p = p->proximo;
+    }  
+    p->proximo = p->proximo->proximo;
+}
+
 TPnodo desenfileirar(TPfila* fila){
     apontador  q;
-    if(estado(fila)){
-        cout<<"\nThe queue is empty.\n";
-    }
     q=fila->frente;
     fila->frente = fila->frente->proximo;
     free(q);
     return fila->frente->nodo;
+}
+
+
+void imprimir(TPfila* fila){
+    if(fila->frente==NULL){
+        cout<<"\nThe queue is empty.\n";
+    }else{
+        cout<<"Values on list: \n";
+        apontador p = (apontador) malloc(sizeof(Celula));
+        for(p=fila->frente->proximo; p!=NULL; p = p->proximo){
+            cout<<p->nodo.chave<<endl;
+        }
+    }
+}
+
+int tamanho(TPfila* fila){
+    int cont = 0;
+    apontador p = (apontador) malloc(sizeof(Celula));
+    if(fila->frente->proximo != NULL){
+        for (p=fila->frente->proximo; p != NULL; p = p->proximo){
+            cont++;
+        }
+    }
+    return cont;
 }
--- SECOND-SEMESTER/Data_Structure_And_Algorithms/Queue_With_Pointer/queue.h
+++ SECOND-SEMESTER/Data_Structure_And_Algorithms/Queue_With_Pointer/queue.h
@@ -18,3 +18,7 @@ void esvaziar(TPfila* );
 bool estado(TPfila*);
 void enfileirar(TPnodo, TPfila*);
 TPnodo desenfileirar(TPfila*);
+void fura_fila_desenfileira(TPnodo, TPfila*);
+void fura_fila_enfileira(TPnodo, TPfila*, int);
+void imprimir(TPfila*);
+int tamanho(TPfila*);
